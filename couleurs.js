be5618b1077188d9c677d7a98fbf804d1cae6090
Mr Game Pro <mrgamepro@gmx.fr>
2020-05-18 11:33:24 +0200

    Update couleurs.js

--- couleurs.js
+++ couleurs.js
@@ -1,1 +1,18 @@
-console.log("Vous venez de lancer l'exercice : \"Couleurs\" !")
+console.log("Vous venez de lancer l'exercice : \"Couleurs\" !\n\n");
+
+let couleurs = [
+    "bleu",
+    "vert",
+    "jaune",
+    "rouge"
+];
+
+for (let i=0; i<4; i++) {
+    console.log(`L'index ${i} donne la couleur ${couleurs[i]}\n`);
+    switch(couleurs[i]) {
+        case "bleu":
+            console.log("La couleur est : bleu");
+        default:
+            console.log("Pas de couleur ;)");
+    }
+} 
