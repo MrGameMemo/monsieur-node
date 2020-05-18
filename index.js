4feab9a33499eb346e14237d6ef7b5770aa1e267
Mr Game Pro <mrgamepro@gmx.fr>
2020-05-18 11:00:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ console.log("Choisis un des exercices suivants pour commencer :\n    - Couleurs"
 let exerciceALancer = process.stdin;
 exerciceALancer.setEncoding("utf-8");
 exerciceALancer.on("data", function (data) {
-if (data === "couleurs"){
+if (data === "couleurs\n"){
+    console.log("coucou !");
     require("./couleurs");
     return;
 }
